837f9dc668ba452a12f7aceee9fb579415a2ee15
Unknown <jake.reed1@gmail.com>
2018-05-18 16:27:07 -0600

    Add basic form data to `newspaper_page`.

--- app/forms/hyrax/newspaper_page_form.rb
+++ app/forms/hyrax/newspaper_page_form.rb
@@ -4,7 +4,13 @@ module Hyrax
   # Newspaper Page Form Class
   class NewspaperPageForm < Hyrax::Forms::WorkForm
     self.model_class = ::NewspaperPage
-    self.required_fields = [:height, :width]
-    self.terms += [:resource_type]
+    self.terms += [:label, :height, :width, :resource_type, :text_direction,
+                   :page_number, :section]
+    self.terms -= [:title, :creator, :keyword, :rights_statement, :contributor,
+                   :description, :license, :subject, :date_created, :subject,
+                   :language, :based_near, :related_url, :source,
+                   :resource_type, :publisher]
+    self.required_fields += [:label, :height, :width]
+    self.required_fields -= [:title, :creator, :keyword, :rights_statement]
   end
 end
