07129571cfcba71e619f44655a1bd138bed841e6
Unknown <jake.reed1@gmail.com>
2018-05-18 15:00:51 -0600

    Add basic form data for `newspaper_container`

--- app/forms/hyrax/newspaper_container_form.rb
+++ app/forms/hyrax/newspaper_container_form.rb
@@ -4,7 +4,11 @@ module Hyrax
   # Newspaper Container Form Class
   class NewspaperContainerForm < Hyrax::Forms::WorkForm
     self.model_class = ::NewspaperContainer
-    self.required_fields = [:resource_type, :genre, :language, :held_by]
-    self.terms += [:resource_type]
+    self.terms += [:resource_type, :held_by, :place_of_publication,
+                   :alternative_title, :issued, :issn, :lccn, :oclcnum, :extent]
+    self.terms -= [:creator, :keyword, :contributor, :description, :location,
+                   :related_url, :date_created, :subject, :based_near, :source]
+    self.required_fields += [:resource_type, :genre, :language, :held_by]
+    self.required_fields -= [:creator, :keyword, :rights_statement]
   end
 end
