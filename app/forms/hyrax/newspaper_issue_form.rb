a5a1e1f0c2781e1b78e2bb222766736399d3a3ae
Unknown <jake.reed1@gmail.com>
2018-05-18 15:20:05 -0600

    Add basic form data for `newspaper_issue`

--- app/forms/hyrax/newspaper_issue_form.rb
+++ app/forms/hyrax/newspaper_issue_form.rb
@@ -4,8 +4,12 @@ module Hyrax
   # Newspaper Issue Form Class
   class NewspaperIssueForm < Hyrax::Forms::WorkForm
     self.model_class = ::NewspaperIssue
-    self.terms += [:title, :resource_type, :genre, :language, :held_by, :issued,
-                   :alternative_title]
-    self.required_fields = [:title, :resource_type, :genre, :language, :held_by]
+    self.terms += [:alternative_title, :resource_type, :genre,
+                   :place_of_publication, :volume, :edition, :issue, :language,
+                   :issn, :lccn, :oclcnum, :extent, :held_by, :issued]
+    self.terms -= [:creator, :keyword, :related_url, :source, :description,
+                   :contributor, :date_created, :subject, :based_near]
+    self.required_fields += [:resource_type, :genre, :language, :held_by]
+    self.required_fields -= [:creator, :rights_statement, :keyword]
   end
 end
