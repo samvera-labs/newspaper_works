d406ad2a314317b3114e9b50e90f877e9213e80c
Unknown <jake.reed1@gmail.com>
2018-05-18 14:40:24 -0600

    Complete basic form info for `newspaper_title`

--- app/forms/hyrax/newspaper_title_form.rb
+++ app/forms/hyrax/newspaper_title_form.rb
@@ -5,8 +5,11 @@ module Hyrax
   class NewspaperTitleForm < Hyrax::Forms::WorkForm
     self.model_class = ::NewspaperTitle
     self.terms += [:alternative_title, :resource_type, :genre, :held_by, :issued,
-                   :place_of_publication, :edition, :frequency, :issn, :lccn, :oclcnum]
-    self.required_fields += [:title, :resource_type, :genre, :language, :held_by]
-    self.required_fields -= [:rights]
+                   :place_of_publication, :edition, :frequency, :issn, :lccn,
+                   :oclcnum, :preceded_by, :succeeded_by]
+    self.terms -= [:contributor, :creator, :keyword, :related_url, :description,
+                   :date_created, :subject, :based_near, :source]
+    self.required_fields += [:resource_type, :genre, :language, :held_by]
+    self.required_fields -= [:rights_statement, :creator, :keyword]
   end
 end
