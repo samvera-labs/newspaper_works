7ae613fcaedec1ba8a67c8fb9877d91d997671b8
Unknown <jake.reed1@gmail.com>
2018-05-21 09:47:12 -0600

    Add basic form functionality for `newspaper_articles`

--- app/forms/hyrax/newspaper_article_form.rb
+++ app/forms/hyrax/newspaper_article_form.rb
@@ -3,8 +3,12 @@
 module Hyrax
   class NewspaperArticleForm < Hyrax::Forms::WorkForm
     self.model_class = ::NewspaperArticle
-    self.terms += [:title, :resource_type, :genre, :language, :held_by, :issued,
-                   :alternative_title]
-    self.required_fields = [:title, :resource_type, :genre, :language, :held_by]
+    self.terms += [:subtitle, :author, :photographer, :resource_type, :genre,
+                   :place_of_publication, :volume, :edition, :issue, :section,
+                   :subject, :geographic_coverage, :issn, :lccn, :oclcnum,
+                   :extent, :pagination, :held_by, :section]
+    self.terms -= [:keyword]
+    self.required_fields += [:resource_type, :genre, :language, :held_by]
+    self.required_fields -= [:creator, :keyword, :rights_statement]
   end
 end
