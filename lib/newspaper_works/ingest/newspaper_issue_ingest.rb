ccc87bceb808ab285aa40cf94ed2c57436ab1472
Sean Upton <sdupton@gmail.com>
2018-06-06 15:16:39 -0600

    Namespace PdfPages usage, copy issue depositor to child work (page) depositor.

--- lib/newspaper_works/ingest/newspaper_issue_ingest.rb
+++ lib/newspaper_works/ingest/newspaper_issue_ingest.rb
@@ -5,7 +5,7 @@ module NewspaperWorks
         # first, handle the PDF itself on the issue...
         super
         # ...then create child works from split pages
-        pages = PdfPages(path)
+        pages = NewspaperWorks::Ingest::PdfPages.new(path)
         pages.each_with_index do |tiffpath, idx|
           new_child_page_with_file(tiffpath, idx)
         end
@@ -14,6 +14,7 @@ module NewspaperWorks
       def new_child_page_with_file(tiffpath, idx)
         page = NewspaperPage.new
         page.title = [format("Page %<pagenum>i", pagenum: idx + 1)]
+        page.depositor = @work.depositor
         page.save!
         @work.members.push(page)
         @work.save!
