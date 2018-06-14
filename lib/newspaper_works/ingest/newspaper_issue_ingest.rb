087cdf229ec5710571b489f04bc87d16c01d373a
Sean Upton <sdupton@gmail.com>
2018-06-13 22:57:07 -0600

    Independent method to create child NewspaperPage works with TIFF for NewspaperIssueIngest, purpose of which is to be called by job triggered by Hyrax upload, which is already handling attachment of PDF to issue.

--- lib/newspaper_works/ingest/newspaper_issue_ingest.rb
+++ lib/newspaper_works/ingest/newspaper_issue_ingest.rb
@@ -5,6 +5,14 @@ module NewspaperWorks
         # first, handle the PDF itself on the issue...
         super
         # ...then create child works from split pages
+        create_child_pages
+      end
+
+      # Creates child pages with attached TIFF masters, can be called by
+      #   `import`, or independently if `load` is called first.  The
+      #   latter is appropriate if framework is already handling the
+      #   NewspaperIssue file attachment (e.g. Hyrax upload via browser).
+      def create_child_pages
         pages = NewspaperWorks::Ingest::PdfPages.new(path)
         pages.each_with_index do |tiffpath, idx|
           new_child_page_with_file(tiffpath, idx)
