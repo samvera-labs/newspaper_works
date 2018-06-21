7c1d929f50dd353a093a375ff2bb6c97741f1051
Sean Upton <sdupton@gmail.com>
2018-06-21 09:04:10 -0600

    uploaded_pdf_file factory attempts to set a user to make Rails 5.1 happy

--- spec/factories/uploaded_pdf_file.rb
+++ spec/factories/uploaded_pdf_file.rb
@@ -3,7 +3,7 @@ FactoryBot.define do
     initialize_with do
       base = File.join(NewspaperWorks::GEM_PATH, 'spec', 'fixtures', 'files')
       pdf_path = File.join(base, 'sample-color-newsletter.pdf')
-      new(file: File.open(pdf_path))
+      new(file: File.open(pdf_path), user: create(:user))
     end
   end
 end
