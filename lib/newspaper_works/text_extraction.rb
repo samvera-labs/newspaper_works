c567861a0f7669d5093b16decde0e58fb273ea1c
Sean Upton <sdupton@gmail.com>
2018-08-07 14:21:16 -0600

    Revert "ALTO to IIIF transform adapter, via Glen Robson's XSLT transform for"
    This reverts commit 89b666fb367cd1134921dc87ef40c3f1aec5e228.

--- lib/newspaper_works/text_extraction.rb
+++ lib/newspaper_works/text_extraction.rb
@@ -1,6 +1,5 @@
 require 'newspaper_works/text_extraction/page_ocr'
 require 'newspaper_works/text_extraction/render_alto'
-require 'newspaper_works/text_extraction/alto_to_iiif'
 
 module NewspaperWorks
   # Module for text extraction (OCR or otherwise)
--- spec/services/newspaper_works/text_extraction_derivative_service_spec.rb
+++ spec/services/newspaper_works/text_extraction_derivative_service_spec.rb
@@ -1,4 +1,3 @@
-require 'json'
 require 'nokogiri'
 require 'spec_helper'
 
@@ -20,7 +19,7 @@ RSpec.describe NewspaperWorks::TextExtractionDerivativeService do
     )
   end
 
-  describe "Creates OCR derivatives" do
+  describe "Creates ALTO derivative" do
     def source_image(name)
       File.join(fixture_path, name)
     end
@@ -33,26 +32,16 @@ RSpec.describe NewspaperWorks::TextExtractionDerivativeService do
       altoxsd.validate(filename)
     end
 
-    def derivative_exists(ext)
-      path = expected_path(valid_file_set, ext)
-      expect(File.exist?(path)).to be true
-      expect(File.size(path)).to be > 0
-    end
-
-    it "creates, stores valid ALTO, plain, IIIF derivatives" do
+    it "creates, stores valid ALTO and plain-text derivatives" do
       # these are in same test to avoid duplicate OCR operation
       service = described_class.new(valid_file_set)
       service.create_derivatives(source_image('ocr_mono.tiff'))
       # ALTO derivative file exists at expected path and validates:
       altoxsd.validate(expected_path(valid_file_set, 'xml'))
       # Plain text exists as non-empty file:
-      derivative_exists('txt')
-      # IIIF annotation list:
-      derivative_exists('json')
-      iiif_path = expected_path(valid_file_set, 'json')
-      result = File.read(iiif_path)
-      loaded_result = JSON.parse(result)
-      expect(loaded_result['resources'].length).to be > 0
+      txt_path = expected_path(valid_file_set, 'txt')
+      expect(File.exist?(txt_path)).to be true
+      expect(File.size(txt_path)).to be > 0
     end
   end
 end
