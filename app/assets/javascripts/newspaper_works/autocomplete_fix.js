8d70f79363d5e2d9bb26981578011a9b61a05e96
Sean Upton <sdupton@gmail.com>
2018-08-16 14:33:38 -0600

    Reinitialize autocomplete fields in JS

--- app/assets/javascripts/newspaper_works/autocomplete_fix.js
+++ app/assets/javascripts/newspaper_works/autocomplete_fix.js
@@ -2,18 +2,22 @@
 
 (function () {
   "use strict";
+  var work_form = $("form[data-behavior='work-form']"),
+      Autocomplete = require('hyrax/autocomplete'),
+      LinkedData = require('hyrax/autocomplete/linked_data');
   
   function patch_autocomplete() {
-    var method = require('hyrax/autocomplete').prototype.setup;
+    var method = Autocomplete.prototype.setup;
     if (!window._autocomplete_patched) {
-      require('hyrax/autocomplete').prototype.setup = function (element, fieldName, url) {
-        var defer = ['based_near'],
+      Autocomplete.prototype.setup = function (element, fieldName, url) {
+        // defer to original method for relevant BasicMetadata fields:
+        var defer = ['based_near', 'language'],
             autourl = element.attr('data.autocomplete-url'),
             declared = (element.attr('data-autocomplete') === fieldName),
             ld = (declared && autourl && autourl.indexOf('authorities') !== -1);
         if (declared && defer.indexOf(fieldName) == -1) {
           new LinkedData(element, url);
-          console.log('Patched autocomplete for field', fieldName);
+          console.log('Using patched autocomplete for field', fieldName);
         } else {
           method(element, fieldName, url); 
         }
@@ -22,9 +26,27 @@
     }
   }
 
-  var work_form = $("form[data-behavior='work-form']");
+  function reinitialize_autocomplete() {
+    $("input[data-autocomplete]").each(function() {
+      var input = $(this),
+          autocomplete;
+      if ($(input).data('autocomplete')) {
+        // remove jqueryui autocomplete previously set up:
+        $(input).autocomplete("destroy");
+        // re-initialize Hyrax autocomplete:
+        autocomplete = new Autocomplete();
+        autocomplete.setup(
+          input,
+          input.attr('data-autocomplete'),
+          input.attr('data-autocomplete-url')
+        );
+      }
+    });
+  }
+
   if (work_form.length && work_form.attr('id').indexOf('newspaper_') != -1) {
     patch_autocomplete();
+    reinitialize_autocomplete();
   }
 }());
 
