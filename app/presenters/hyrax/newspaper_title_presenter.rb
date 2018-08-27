69b7902731397756042cb6a2f50b58ecc1268ae9
Jacob Reed <jacob@xanadu.uconnect.utah.edu>
2018-08-27 11:46:26 -0600

    Add additional details to show

--- app/presenters/hyrax/newspaper_title_presenter.rb
+++ app/presenters/hyrax/newspaper_title_presenter.rb
@@ -2,5 +2,8 @@
 #  `rails generate hyrax:work NewspaperTitle`
 module Hyrax
   class NewspaperTitlePresenter < Hyrax::WorkShowPresenter
+    delegate :edition, :frequency, :preceded_by, :succeeded_by,
+             :publication_date_start, :publication_date_end,
+             to: :solr_document
   end
 end
