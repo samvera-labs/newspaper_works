e998e5fed8a4162af049bb3f8e7888f09fcb6ed0
Eben English <eenglish@bpl.org>
2018-09-25 14:01:01 -0400

    refactor to allow use of newspaper_works/base partials to override Hyrax views for Newspaper* objects

--- app/presenters/newspaper_works/newspaper_core_presenter.rb
+++ app/presenters/newspaper_works/newspaper_core_presenter.rb
@@ -2,7 +2,7 @@
 module NewspaperWorks
   # core presenter for newspaper models
   module NewspaperCorePresenter
-    delegate :alternative_title, :genre, :place_of_publication,
-             :issn, :lccn, :oclcnum, :held_by, to: :solr_document
+    delegate :alternative_title, :genre, :issn, :lccn,
+             :oclcnum, :held_by, to: :solr_document
   end
 end
