dec70aa93a7f9a3d23b159143fa2ae9340f304c4
Sean Upton <sdupton@gmail.com>
2018-09-25 09:45:11 -0600

    DRY fixes for tests of WorkDerivativeLoader

--- spec/lib/newspaper_works/data/work_derivative_loader_spec.rb
+++ spec/lib/newspaper_works/data/work_derivative_loader_spec.rb
@@ -4,29 +4,27 @@ require 'misc_shared'
 RSpec.describe NewspaperWorks::Data::WorkDerivativeLoader do
   include_context "shared setup"
 
+  let(:work) do
+    # sample work comes from shared setup, but we need derivative, save...
+    mk_txt_derivative(sample_work)
+    sample_work.save!(validate: false)
+    sample_work
+  end
+
   describe "enumerates available derivatives" do
     it "includes expected derivative path for work" do
-      work = sample_work
-      mk_txt_derivative(work)
-      work.save!(validate: false)
       loader = described_class.new(work)
       ext_found = loader.paths.map { |v| v.split('.')[-1] }
       expect(ext_found).to include 'txt'
     end
 
     it "enumerates expected derivative extension for work" do
-      work = sample_work
-      mk_txt_derivative(work)
-      work.save!(validate: false)
       loader = described_class.new(work)
       ext_found = loader.to_a
       expect(ext_found).to include 'txt'
     end
 
     it "enumerates expected derivative extension for file set" do
-      work = sample_work
-      mk_txt_derivative(work)
-      work.save!(validate: false)
       file_set = work.members.select { |m| m.class == FileSet }[0]
       loader = described_class.new(file_set)
       ext_found = loader.to_a
@@ -34,9 +32,6 @@ RSpec.describe NewspaperWorks::Data::WorkDerivativeLoader do
     end
 
     it "enumerates expected derivative extension for file set id" do
-      work = sample_work
-      mk_txt_derivative(work)
-      work.save!(validate: false)
       file_set = work.members.select { |m| m.class == FileSet }[0]
       loader = described_class.new(file_set.id)
       ext_found = loader.to_a
@@ -46,25 +41,17 @@ RSpec.describe NewspaperWorks::Data::WorkDerivativeLoader do
 
   describe "loads derivatives for a work" do
     it "Loads text derivative path" do
-      work = sample_work
-      mk_txt_derivative(work)
-      work.save!(validate: false)
       loader = described_class.new(work)
       expect(File.exist?(loader.path('txt'))).to be true
     end
 
     it "Loads text derivative data" do
-      work = sample_work
-      mk_txt_derivative(work)
-      work.save!(validate: false)
       loader = described_class.new(work)
       expect(loader.data('txt')).to include 'mythical'
     end
 
     it "Can access jp2 derivative" do
-      work = sample_work
       mk_jp2_derivative(work)
-      work.save!(validate: false)
       loader = described_class.new(work)
       expect(File.exist?(loader.path('jp2'))).to be true
     end
