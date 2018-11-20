d215207c6a39c9ead825ec74c47b283b99aa2343
Eben English <eenglish@bpl.org>
2018-11-20 11:45:04 -0500

    customize NewspaperIssuePresenter#universal_viewer?
    should return true if the Issue has child Page images

--- app/presenters/hyrax/newspaper_issue_presenter.rb
+++ app/presenters/hyrax/newspaper_issue_presenter.rb
@@ -2,5 +2,21 @@
 #  `rails generate hyrax:work NewspaperIssue`
 module Hyrax
   class NewspaperIssuePresenter < Hyrax::WorkShowPresenter
+    # @return [Boolean] render the UniversalViewer
+    def universal_viewer?
+      Hyrax.config.iiif_image_server? && members_include_viewable_page?
+    end
+
+    private
+
+      # modeled on Hyrax::WorkShowPresenter#members_include_viewable_image?
+      # @return [Boolean] whether the member works will show in the IIIF viewer
+      def members_include_viewable_page?
+        work_presenters.any? do |presenter|
+          presenter.model_name == 'NewspaperPage' &&
+            presenter.universal_viewer? &&
+            current_ability.can?(:read, presenter.id)
+        end
+      end
   end
 end
