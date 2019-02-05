d8774285eca295b0d31cb4f2a41fcf46d3a01406
Jacob Reed <jake.reed1@gmail.com>
2019-02-05 11:57:41 -0700

    Update title page to show all issues for a year

--- app/presenters/hyrax/newspaper_title_presenter.rb
+++ app/presenters/hyrax/newspaper_title_presenter.rb
@@ -5,16 +5,12 @@ module Hyrax
     include NewspaperWorks::NewspaperCorePresenter
     delegate :edition, :frequency, :preceded_by, :succeeded_by, to: :solr_document
 
-    def members
-      NewspaperTitle.find(solr_document.id).members
-    end
-
-    def member_publication_dates
-      dates = []
-      members.each do |issue|
-        dates << issue.publication_date if issue.class == NewspaperIssue
+    def issues
+      issue_list = []
+      members.each do |member|
+        issue_list << member if member.class == NewspaperIssue
       end
-      dates
+      issue_list
     end
 
     def publication_date_start
@@ -24,5 +20,11 @@ module Hyrax
     def publication_date_end
       solr_document["publication_date_end_dtsim"]
     end
+
+    private
+
+      def members
+        NewspaperTitle.find(solr_document.id).members
+      end
   end
 end
