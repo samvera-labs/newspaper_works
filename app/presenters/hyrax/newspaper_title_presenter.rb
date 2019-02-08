a9f871f264e93fe23e95106f2013203c5f052070
Jacob Reed <jake.reed1@gmail.com>
2019-02-07 17:52:00 -0700

    Update title view to allow navigation between issue years

--- app/presenters/hyrax/newspaper_title_presenter.rb
+++ app/presenters/hyrax/newspaper_title_presenter.rb
@@ -6,11 +6,25 @@ module Hyrax
     delegate :edition, :frequency, :preceded_by, :succeeded_by, to: :solr_document
 
     def issues
-      issue_list = []
-      members.each do |member|
-        issue_list << member if member.class == NewspaperIssue
-      end
-      issue_list
+      all_title_issues.select { |issue| Date.parse(issue.publication_date).year == year }
+    end
+
+    def issue_dates
+      issues.pluck(:publication_date)
+    end
+
+    def issue_years
+      all_title_issue_dates.map { |issue| Date.parse(issue).year }.uniq.sort
+    end
+
+    def prev_year
+      index = issue_years.index(year) - 1
+      return nil if index < 0
+      issue_years[index]
+    end
+
+    def next_year
+      issue_years[issue_years.index(year) + 1]
     end
 
     def publication_date_start
@@ -21,10 +35,25 @@ module Hyrax
       solr_document["publication_date_end_dtsim"]
     end
 
+    def year
+      number_or_nil(request.params[:year]) || issue_years.first
+    end
+
     private
 
-      def members
-        NewspaperTitle.find(solr_document.id).members
+      def all_title_issues
+        members = NewspaperTitle.find(solr_document.id).members
+        members.select { |member| member.class == NewspaperIssue }
+      end
+
+      def all_title_issue_dates
+        all_title_issues.pluck(:publication_date)
+      end
+
+      def number_or_nil(string)
+        Integer(string || '')
+      rescue ArgumentError
+        nil
       end
   end
 end
