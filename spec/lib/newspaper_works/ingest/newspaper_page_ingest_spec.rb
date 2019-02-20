47cdfd3af845cffb8a55cf7570b90a71a26f460c
Sean Upton <sdupton@gmail.com>
2019-02-20 15:13:46 -0800

    NewspaperWorks::Ingest::PageIngest and IssueIngest (PDF) adapters now use NewspaperWorks::Data::WorkFiles (and thus actor stack) for attachment.
    This is instead of using only Hyrax's OOTB AttachFilesToWorkJob, which
    was incomplete at several aspects of setting technical metadata, among
    other problems.  This does make a minor shift from expecting that
    attachment is to be peformed now, to performed eventually.

--- spec/lib/newspaper_works/ingest/newspaper_page_ingest_spec.rb
+++ spec/lib/newspaper_works/ingest/newspaper_page_ingest_spec.rb
@@ -1,32 +1,45 @@
 require 'spec_helper'
+require 'misc_shared'
 require 'faraday'
 
 # test NewspaperPageIngest against work
 RSpec.describe NewspaperWorks::Ingest::NewspaperPageIngest do
+  include_context "shared setup"
+
   # define the path to the file we will use for multiple examples
   let(:path) do
-    fixtures = File.join(NewspaperWorks::GEM_PATH, 'spec/fixtures/files')
-    File.join(fixtures, 'page1.tiff')
+    File.join(fixture_path, 'page1.tiff')
   end
 
   it_behaves_like('ingest adapter IO')
 
   describe "file import and attachment" do
-    it "ingests file data and saves" do
+    do_now_jobs = [IngestLocalFileJob, IngestJob]
+
+    def verify_pcdm_fileset(fileset)
+      # Hyrax always sets label (if not title) on fileset:
+      expect(fileset.label).to eq 'page1.tiff'
+      # reload file set and check on original file
+      fileset.reload
+      file = fileset.original_file
+      expect(file).to be_a Hydra::PCDM::File
+    end
+
+    def verify_attached_file(work, path)
+      work.reload
+      files = NewspaperWorks::Data::WorkFiles.of(work)
+      expect(files.keys.size).to eq 1
+      expect(File.exist?(files.values[0].path)).to be true
+      expect(files.values[0].size).to eq File.size(path)
+    end
+
+    it "ingests file data and saves", perform_enqueued: do_now_jobs do
       adapter = build(:newspaper_page_ingest)
-      # Rails.application.config.active_job.queue_adapter = :inline
       adapter.ingest(path)
-      # Rails.application.config.active_job.queue_adapter = :async
       file_sets = adapter.work.members.select { |w| w.class == FileSet }
-      expect(file_sets[0].title).to contain_exactly 'page1.tiff'
       expect(file_sets.size).to eq 1
-      # TODO: re-enable this if we can come to a reliable way to run all
-      #   the stuff Hyrax does via AttachFilesToWorkJob inline:
-      # files = file_sets[0].files
-      # expect(files.size).to eq 1
-      # url = files[0].uri.to_s
-      # stored_size = Faraday.get(url).body.length
-      # expect(stored_size).to eq File.size(path)
+      verify_pcdm_fileset(file_sets[0])
+      verify_attached_file(adapter.work, path)
     end
   end
 end
