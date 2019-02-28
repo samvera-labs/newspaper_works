735e1a56170e0bf404e8f1527b446a1e909dd257
Sean Upton <sdupton@gmail.com>
2019-02-27 23:50:23 -0700

    Tests for permissions on fileset match page on NewspaperPageIngest.

--- spec/lib/newspaper_works/ingest/newspaper_page_ingest_spec.rb
+++ spec/lib/newspaper_works/ingest/newspaper_page_ingest_spec.rb
@@ -14,7 +14,21 @@ RSpec.describe NewspaperWorks::Ingest::NewspaperPageIngest do
   it_behaves_like('ingest adapter IO')
 
   describe "file import and attachment" do
-    do_now_jobs = [IngestLocalFileJob, IngestJob]
+    do_now_jobs = [
+      IngestJob,
+      IngestLocalFileJob,
+      InheritPermissionsJob,
+      VisibilityCopyJob
+    ]
+
+    permission_methods = [
+      :edit_users,
+      :read_users,
+      :discover_users,
+      :edit_groups,
+      :read_groups,
+      :discover_groups
+    ]
 
     def verify_pcdm_fileset(fileset)
       # Hyrax always sets label (if not title) on fileset:
@@ -40,6 +54,24 @@ RSpec.describe NewspaperWorks::Ingest::NewspaperPageIngest do
       expect(file_sets.size).to eq 1
       verify_pcdm_fileset(file_sets[0])
       verify_attached_file(adapter.work, path)
+    end
+
+    # For (minimal) page objects constructed in tests, PDF ingest,
+    #   permission setting on fileset happens as result of attachment
+    #   process (`NewspaperWorks::Data::WorkFiles`), via the
+    #   `NewspaperWorks::Data::handle_after_create_fileset` method,
+    #   since the CreateWithRemoteFilesActor in Hyrax unfortunately
+    #   does not invoke InheritPermissionJob.
+    it "copies work permissions to fileset", perform_enqueued: do_now_jobs do
+      adapter = build(:newspaper_page_ingest)
+      adapter.ingest(path)
+      adapter.work.reload
+      file_sets = adapter.work.members.select { |w| w.class == FileSet }
+      fileset = file_sets[0]
+      permission_methods.each do |m|
+        expect(fileset.send(m)).to match_array adapter.work.send(m)
+      end
+      expect(fileset.visibility).to eq adapter.work.visibility
     end
   end
 end
