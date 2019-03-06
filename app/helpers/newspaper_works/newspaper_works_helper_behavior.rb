52367e4a7568cc56bddcaf06ad56b4d2d0bd8f34
Eben English <eenglish@bpl.org>
2019-03-06 16:45:11 -0500

    specs for highlight behavior; refactor #render_newspaper_thumbnail_tag for Rubocop happiness

--- app/helpers/newspaper_works/newspaper_works_helper_behavior.rb
+++ app/helpers/newspaper_works/newspaper_works_helper_behavior.rb
@@ -12,31 +12,38 @@ module NewspaperWorks
 
     ##
     # based on Blacklight::CatalogHelperBehavior#render_thumbnail_tag
-    # Render the thumbnail, if available, for a NewspaperPage or Article
-    # link it to the document record, with anchor for UV IIIF search
+    # setup the thumbnail link for a NewspaperPage or Article
     #
     # @param document [SolrDocument]
     # @param query [String]
     # @return [String]
     def render_newspaper_thumbnail_tag(document, query)
-      value = if blacklight_config.view_config(document_index_view_type).thumbnail_method
-                send(blacklight_config.view_config(document_index_view_type).thumbnail_method,
-                     document,
-                     image_options)
-              elsif blacklight_config.view_config(document_index_view_type).thumbnail_field
-                image_tag blacklight_config.view_config(document_index_view_type).thumbnail_field
-              end
-      if value
-        case document[blacklight_config.index.display_type_field.to_sym].first
-        when 'NewspaperPage'
-          link_to(value, hyrax_newspaper_page_path(document.id,
-                                                   anchor: iiif_search_anchor(query)))
-        when 'NewspaperArticle'
-          link_to(value, hyrax_newspaper_article_path(document.id,
-                                                      anchor: iiif_search_anchor(query)))
-        else
-          link_to_document document, value
-        end
+      thumbnail = newspaper_thumbnail_tag(document)
+      return unless thumbnail
+      anchor = iiif_search_anchor(query)
+      case document[blacklight_config.view_config(document_index_view_type).display_type_field].first
+      when 'NewspaperPage'
+        link_to(thumbnail, hyrax_newspaper_page_path(document.id, anchor: anchor))
+      when 'NewspaperArticle'
+        link_to(thumbnail, hyrax_newspaper_article_path(document.id, anchor: anchor))
+      else
+        link_to_document document, thumbnail
+      end
+    end
+
+    ##
+    # based on Blacklight::CatalogHelperBehavior#render_thumbnail_tag
+    # return the thumbnail image_tag
+    #
+    # @param document [SolrDocument]
+    # @return [String]
+    def newspaper_thumbnail_tag(document)
+      if blacklight_config.view_config(document_index_view_type).thumbnail_method
+        send(blacklight_config.view_config(document_index_view_type).thumbnail_method,
+             document)
+      elsif blacklight_config.view_config(document_index_view_type).thumbnail_field
+        url = thumbnail_url(document)
+        image_tag url if url.present?
       end
     end
   end
