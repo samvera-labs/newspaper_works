744da1c8230e9afd37228b27ba03817cfefb4b2b
Jacob Reed <jake.reed1@gmail.com>
2019-03-25 15:31:12 -0600

    Add generator to automatically install css assets

---
+++ lib/generators/newspaper_works/assets_generator.rb
@@ -0,0 +1,11 @@
+require 'rails/generators'
+
+class Hyrax::AssetsGenerator < Rails::Generators::Base
+  desc "This generator installs the newspaper_works CSS assets into your application"
+
+  source_root File.expand_path('../templates', __FILE__)
+
+  def inject_css
+    copy_file "newspaper_works.scss", "app/assets/stylesheets/newspaper_works.scss"
+  end
+end
