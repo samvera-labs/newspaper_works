612ab94fefd72d48af36906bc091a52b2c1c5afb
Eben English <eenglish@bpl.org>
2019-05-07 14:44:27 -0400

    add date range field to form; initial style improvements

--- lib/generators/newspaper_works/templates/custom_search_builder.rb
+++ lib/generators/newspaper_works/templates/custom_search_builder.rb
@@ -2,4 +2,10 @@
 class CustomSearchBuilder < Hyrax::CatalogSearchBuilder
   include BlacklightAdvancedSearch::AdvancedSearchBuilder
   self.default_processor_chain += [:add_advanced_parse_q_to_solr, :add_advanced_search_to_solr]
+
+  # add logic to BlacklightAdvancedSearch::AdvancedSearchBuilder
+  # so that date range params are recognized as advanced search
+  def is_advanced_search?
+    blacklight_params[:date_start].present? || blacklight_params[:date_end].present? || super
+  end
 end
