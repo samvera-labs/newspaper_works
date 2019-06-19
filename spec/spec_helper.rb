a47affea8c55b72dfccbefaf32a45794811d7526
Sean Upton <sdupton@gmail.com>
2019-06-19 10:09:39 -0600

    spec_helper before block for workflow runs in :suite scope, not before each test.
    Addresses an omission in 3d037108fc69d7f9bf32f2ae909533c28dec56e2

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -54,7 +54,7 @@ RSpec.configure do |config|
   config.include EngineRoutes, type: :controller
 
   # ensure Hyrax has active sipity workflow for default admin set:
-  config.before do
+  config.before(:suite) do
     admin_set = AdminSet.find(AdminSet.find_or_create_default_admin_set_id)
     permission_template = admin_set.permission_template
     workflow = permission_template.available_workflows.where(
