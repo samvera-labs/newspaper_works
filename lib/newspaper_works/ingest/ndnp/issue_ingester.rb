ce4078d66cf04c7db32e66fbfb070effa1efee59
Sean Upton <sdupton@gmail.com>
2019-08-26 16:14:57 -0600

    Correct argument for NDNP IssueIngester invocation of ComposeIssuePDFJob

--- lib/newspaper_works/ingest/ndnp/issue_ingester.rb
+++ lib/newspaper_works/ingest/ndnp/issue_ingester.rb
@@ -34,7 +34,7 @@ module NewspaperWorks
         def ingest
           construct_issue
           ingest_pages
-          NewspaperWorks::ComposeIssuePDFJob.perform_later(@issue)
+          NewspaperWorks::ComposeIssuePDFJob.perform_later(@target)
         end
 
         def construct_issue
