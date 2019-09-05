25abaadc796b820b41dd26fd9d5991875138213c
Eben English <eenglish@bpl.org>
2019-09-05 10:01:49 -0400

    fix failing specs; add Coveralls badge

--- app/validators/newspaper_works/publication_date_start_end_validator.rb
+++ app/validators/newspaper_works/publication_date_start_end_validator.rb
@@ -5,8 +5,9 @@ module NewspaperWorks
     DATE_RANGE_REGEX = /\A\d{4}(-((0[1-9])|(1[0-2])))?(-(([0-2][1-9])|3[0-1]))?\z/
 
     def validate(record)
-      return unless record.publication_date_start.present? && record.publication_date_end.present?
-      valid_dates?(record) && start_before_end?(record)
+      start_date = record.publication_date_start
+      end_date = record.publication_date_end
+      valid_dates?(start_date, end_date, record) && start_before_end?(start_date, end_date, record)
     end
 
     private
@@ -19,24 +20,29 @@ module NewspaperWorks
         true
       end
 
-      def start_before_end?(record)
-        end_before_start_msg = "Publication start date must be earlier or the same as end date."
-        pub_start = record.publication_date_start.split("-")
-        pub_end = record.publication_date_end.split("-")
+      def start_before_end?(start_date, end_date, record)
+        return true unless start_date && end_date
+        date_error = "Publication start date must be earlier or the same as end date."
+        pub_start = start_date.split("-")
+        pub_end = end_date.split("-")
         (0..2).each do |i|
           if pub_start[i] && pub_end[i] && pub_end[i] < pub_start[i]
-            record.errors[:publication_date_start] << end_before_start_msg
+            record.errors[:publication_date_start] << date_error
             break
           end
         end
-        !record.errors[:publication_date_start].present?
+        record.errors[:publication_date_start].blank?
       end
 
-      def valid_dates?(record)
-        date_incorrect_msg = "Incorrect Date. Date input should be formatted yyyy[-mm][-dd] and be a valid date."
-        record.errors[:publication_date_start] << date_incorrect_msg unless publication_date_valid?(record.publication_date_start)
-        record.errors[:publication_date_end] << date_incorrect_msg unless publication_date_valid?(record.publication_date_end)
-        record.errors[:publication_date_start].present? || record.errors[:publication_date_end].present?
+      def valid_dates?(start_date, end_date, record)
+        date_error = "Incorrect Date. Date input should be formatted yyyy[-mm][-dd] and be a valid date."
+        if start_date
+          record.errors[:publication_date_start] << date_error unless publication_date_valid?(start_date)
+        end
+        if end_date
+          record.errors[:publication_date_end] << date_error unless publication_date_valid?(end_date)
+        end
+        record.errors[:publication_date_start].blank? && record.errors[:publication_date_end].blank?
       end
   end
 end
