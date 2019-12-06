91e070ee4fe10beb84372775174081c8cd99892e
Sean Upton <sdupton@gmail.com>
2019-12-06 12:34:14 -0700

    Fix date format in title of article-segmented page.

--- lib/newspaper_works/ingest/uu_article_segmented/page_ingest.rb
+++ lib/newspaper_works/ingest/uu_article_segmented/page_ingest.rb
@@ -1,3 +1,5 @@
+require 'date'
+
 module NewspaperWorks
   module Ingest
     module UUArticleSegmented
--- spec/lib/newspaper_works/ingest/uu_article_segmented/page_ingest_spec.rb
+++ spec/lib/newspaper_works/ingest/uu_article_segmented/page_ingest_spec.rb
@@ -1,4 +1,5 @@
 require 'spec_helper'
+require 'date'
 
 RSpec.describe NewspaperWorks::Ingest::UUArticleSegmented::PageIngest do
   include_context 'ingest test fixtures'
@@ -28,7 +29,8 @@ RSpec.describe NewspaperWorks::Ingest::UUArticleSegmented::PageIngest do
       expect(page.page_number).to eq '8'
       expect(page.publication_date).to eq '1850-06-15'
       # title without issue context contains date and page_number:
-      expected_title = "#{page.publication_date}: Page #{page.page_number}"
+      date = DateTime.iso8601(page.publication_date).strftime('%B %-d, %Y')
+      expected_title = "#{date}: Page #{page.page_number}"
       expect(page.title).to contain_exactly expected_title
     end
   end
