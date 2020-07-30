75fbd44adf0ed84cb4e3d990c1193d4ca5cd20e0
Brian McBride <brian.mcbride@utah.edu>
2020-07-29 20:33:05 -0600

    Resolved spec failures pertaining to place_of_publication values passed as scalar rather than multivalued.

--- spec/indexers/newspaper_works/newspaper_core_indexer_spec.rb
+++ spec/indexers/newspaper_works/newspaper_core_indexer_spec.rb
@@ -1,13 +1,13 @@
 require 'spec_helper'
 
 RSpec.describe NewspaperWorks::NewspaperCoreIndexer do
-  let(:geonames_uri) { 'http://sws.geonames.org/4950065/' }
+  let(:geonames_uri) { 'https://sws.geonames.org/4950065/' }
   let(:pop) { Hyrax::ControlledVocabularies::Location.new(geonames_uri) }
   let(:article) do
     NewspaperArticle.new(
       id: 'foo1234',
       title: ['Whatever'],
-      place_of_publication: pop
+      place_of_publication: [pop]
     )
   end
   let(:indexer) { described_class.new(article) }
